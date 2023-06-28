08d923437374cdbf502dfe8081fbbbe9ed0501df
Your Jonathan Paramo <jhonathan-and@outlook.com>
2023-06-28 16:26:39 -0500

    The component is styled along with its functionality.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -26,7 +26,17 @@ const Home = () => {
       >
         <section className='mt-28 text-center'>
           <h1 className={`${specialElite.className} uppercase text-slate-500 xs:text-3xl md:text-5xl`}>buf's sex shop</h1>
-          <InputWithIcon />
+          <InputWithIcon
+            label="Hola"
+            placeholder="mundo"
+            opacity={60}
+            icon={
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
+              </svg>
+            }
+            fullWidth={true}
+          />
         </section>
       </div>
       <h1>Hola Mundo!</h1>
--- src/components/InputWithIcon.jsx
+++ src/components/InputWithIcon.jsx
@@ -1,5 +1,5 @@
 const InputWithIcon = ({
-  type = "text",
+  type = 'text',
   label,
   placeholder,
   fullWidth,
@@ -7,34 +7,34 @@ const InputWithIcon = ({
   size,
   onChange = undefined,
   icon = undefined,
-  backgroundOpacity
+  opacity
 }) => {
   const inputPaddingClass = placeholder ? 'pl-10' : 'pl-3';
 
   return (
-    <div className="relative flex flex-col">
+    <div className='relative flex flex-col'>
       {label && (
         <label
           htmlFor={label}
-          className="mt-1 text-left text-slate-500 text-lg pointer-events-none block"
+          className='mt-1 text-left text-slate-500 text-lg pointer-events-none block'
         >
           {label}
         </label>
       )}
-      <div className="relative mt-1">
-        <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+      <div className={`relative mt-1 ${fullWidth ? '' : 'inline-flex'}`}>
+        <span className='absolute inset-y-0 left-0 pl-3 flex items-center'>
           {icon}
           <svg
-            className="h-5 w-5 text-slate-500"
-            fill="none"
-            viewBox="0 0 20 20"
+            className='h-5 w-5 text-slate-500'
+            fill='none'
+            viewBox='0 0 20 20'
           >
             <path
-              fillRule="evenodd"
-              d="M7 4a1 1 0 0 0-1 1v6a1 1 0 0 0 2 0V5a1 1 0 0 0-1-1zm5
+              fillRule='evenodd'
+              d='M7 4a1 1 0 0 0-1 1v6a1 1 0 0 0 2 0V5a1 1 0 0 0-1-1zm5
                 4a1 1 0 0 0-1-1H8V5a3 3 0 0 1 6 0v6h.01a1 1 0 0 0 0-2H12zm-3
-                6a1 1 0 0 1 0-2h4a1 1 0 0 1 0 2h-4z"
-              clipRule="evenodd"
+                6a1 1 0 0 1 0-2h4a1 1 0 0 1 0 2h-4z'
+              clipRule='evenodd'
             />
           </svg>
         </span>
@@ -42,6 +42,7 @@ const InputWithIcon = ({
           id={label}
           className={
             `pl-25
+            opacity-${opacity ? opacity : 80}
             ${fullWidth ? 'w-full' : ''}
             h-25
             pr-3
@@ -50,9 +51,7 @@ const InputWithIcon = ({
             border
             rounded-full
             focus:outline-none
-            ${size}
-            ${backgroundOpacity}
-            ${backgroundOpacity}
+            w-${size}
             ${inputPaddingClass}`
           }
           type={type}
@@ -61,7 +60,7 @@ const InputWithIcon = ({
         />
       </div>
       {error && (
-        <div className="text-red-500 text-sm text-left mt-1 ml-0 ml-3">{error}</div>
+        <div className='text-red-500 text-sm text-left mt-1 ml-0 ml-3'>{error}</div>
       )}
     </div>
   );
