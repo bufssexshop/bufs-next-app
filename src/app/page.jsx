246ca06989e63f9501a3daf3594a08f605a88a6b
Carlos Valencia <rasfreed@gmail.com>
2023-08-09 19:18:29 -0500

    change background was fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,22 +4,18 @@ import { useEffect, useState } from 'react'
 import { Special_Elite as SpecialElite } from 'next/font/google'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import { keepServerAlive } from '@/helpers/keepServerAlive'
-import useResponsive from '@/hooks/useResponsive'
 import InputText from '@/components/InputText'
 import Image from 'next/image'
 
 const backgroundImages = {
-  desktop: 'https://res.cloudinary.com/dsykiysl8/image/upload/v1691452375/backgrounds/pexels-bedbible-com-11393547_1920_cojbkd.jpg',
-  mobile: 'https://res.cloudinary.com/dsykiysl8/image/upload/v1687658465/backgrounds/background1_ke0jnm.jpg',
-  desktopDark: 'https://res.cloudinary.com/dsykiysl8/image/upload/v1691453329/backgrounds/pexels-alexander-krivitskiy-10970728_icokfm.jpg',
-  mobileDark: 'https://res.cloudinary.com/dsykiysl8/image/upload/v1691453329/backgrounds/pexels-alexander-krivitskiy-10970728_icokfm.jpg'
+  light: 'https://res.cloudinary.com/dsykiysl8/image/upload/v1691452375/backgrounds/pexels-bedbible-com-11393547_1920_cojbkd.jpg',
+  dark: 'https://res.cloudinary.com/dsykiysl8/image/upload/v1691453329/backgrounds/pexels-alexander-krivitskiy-10970728_icokfm.jpg'
 }
 
 const specialElite = SpecialElite({ weight: '400', subsets: ['latin'] })
 
 const Home = () => {
-  const { isMobile } = useResponsive()
-  const [backgroundImage, setBackgroundImage] = useState(backgroundImages.desktop)
+  const [backgroundImage, setBackgroundImage] = useState(backgroundImages.light)
 
   useEffect(() => {
     const intervalTime = 5 * 60 * 1000
@@ -35,10 +31,8 @@ const Home = () => {
 
   const getBackgroundImage = () => {
     const mode = localStorage.getItem('theme')
-    if (!isMobile && mode === 'dark') setBackgroundImage(backgroundImages.desktopDark)
-    if (!isMobile && mode !== 'dark') setBackgroundImage(backgroundImages.desktop)
-    if (isMobile && mode === 'dark') setBackgroundImage(backgroundImages.mobileDark)
-    if (isMobile && mode !== 'dark') setBackgroundImage(backgroundImages.mobile)
+    if (mode === 'dark') setBackgroundImage(backgroundImages.dark)
+    else setBackgroundImage(backgroundImages.light)
   }
 
   useEffect(() => {
@@ -61,6 +55,7 @@ const Home = () => {
             objectFit: 'cover'
           }}
           quality={100}
+          priority
         />
         <section className='relative pt-28 flex flex-col items-center gap-2'>
           <h1 className={`${specialElite.className} uppercase text-slate-500 dark:text-slate-50 xs:text-3xl md:text-5xl`}>buf's sex shop</h1>
